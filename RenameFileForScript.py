6f7edb210792ee1b858043768fff6dde411c10f5
Li, Yi-Fei <realliyifei@outlook.com>
2022-01-04 01:07:31 -0500

    Add in-place rename and auto-update

--- RenameFileForScript.py
+++ RenameFileForScript.py
@@ -9,44 +9,11 @@
 # you can change it to other output formats as you want 
 
 import sys
-from workflow import Workflow3, ICON_INFO
-
-# GitHub repo to check for updates
-UPDATE_SETTINGS = {
-    'github_slug': 'realliyifei/alfred-file-renamer'
-}
-
-# GitHub Issues
-HELP_URL = 'https://github.com/realliyifei/alfred-file-renamer/issues'
 
 # Global Parameters
 connect_char = '-' # hyphen, you can insteadly use underscore, space, etc. 
 stripped_symbols = [':', ',', '-', '_', '+', '=', '?', '|', 
                     '{', '}', '[', ']', '(', ')', '<', '>'] # the symbols to be stripped 
-
-def main(wf):
-    """Run the workflow"""
-    # Update settings format
-    if wf.update_available:
-        # Add a notification to top of Script Filter results
-        wf.add_item('Workflow update is available',
-                    'Press ↩ or ⇥ to install',
-                    autocomplete = 'workflow:update',
-                    valid = False,
-                    icon = ICON_INFO)
-    
-    # Main    
-    try:
-        filename = wf.args[0] 
-        
-        subtitle = "Press 'Enter' and copy to clipboard "
-        renamed_filename = rename(filename)
-        wf.add_item(title = renamed_filename, subtitle = subtitle, arg = renamed_filename, valid = True)
-        
-    except:
-        wf.add_item("Invalid input.")
-    
-    wf.send_feedback() 
 
 def rename(filename):
     """
@@ -69,12 +36,7 @@ def rename(filename):
     renamed_filename = filename.replace(' ', connect_char)
     
     return renamed_filename 
-    
 
 if __name__ == '__main__':
-    # create a workflow3 object
-    wf = Workflow3(
-        update_settings = UPDATE_SETTINGS,
-        help_url = HELP_URL,
-    ) 
-    sys.exit(wf.run(main)) 
+    filename = ' '.join(sys.argv[1:])
+    print(rename(filename))
